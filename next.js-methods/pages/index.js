60b77c538b1c28fa05a3ac6698cf04d7d5cff817
wpzheng <569359120@qq.com>
2017-11-12 16:27:27 +0800

    remove node_moduels

--- next.js-methods/pages/index.js
+++ next.js-methods/pages/index.js
@@ -3,6 +3,15 @@ import Link from 'next/link'
 
 import Header from '../component/Header'
 
+const PostLink = (props) => {
+  return (
+    <li>
+      <Link href={`/post?title=${post.title}`}>
+        <a>{props.title}</a>
+      </Link>
+    </li>
+  )
+}
 
 const Index = () => {
   return(
@@ -12,6 +21,10 @@ const Index = () => {
         <button>Go to About Page</button>
       </Link>
       <p>Hello Next.js</p>
+      <ul>
+        <PostLink title="Hello Next"/>
+        <PostLink title="apps Next" />
+      </ul>
     </div>
   )
 }
